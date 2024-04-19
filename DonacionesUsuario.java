d95febf4a6b85c056ded0bf54401e7850de40631
danilomedina12 <167130913+danilomedina12@users.noreply.github.com>
2024-04-19 17:06:58 -0300

    Update DonacionesUsuario.java

--- DonacionesUsuario.java
+++ DonacionesUsuario.java
@@ -3,13 +3,35 @@ package sistemaCPM;
 import java.time.*;
 
 public class DonacionesUsuario {
+	private static int nextId = 1;
+	
 	int id;
 	int cantidad;
 	String ubicacion;
 	LocalDate fechaDisponibilidad;
 	int usuarioDonante;
 	
+	public DonacionesUsuario(int cantidad, String ubicacion, LocalDate fechaDisponibilidad, int usuarioDonante){
+		this.id = nextId++;
+		this.cantidad = cantidad; 
+		this.ubicacion = ubicacion;
+		this.fechaDisponibilidad = fechaDisponibilidad;
+		this.usuarioDonante = usuarioDonante;
+		}
+	
 	public void actualizarCantidad(int nuevaCantidad) {
 		this.cantidad = nuevaCantidad;
+	}
+	
+	public int obtenerCantidad() {
+		return this.cantidad;
+	}
+
+	public LocalDate obtenerFechaDisponibilidad() {
+		return this.fechaDisponibilidad;
+	}
+
+	public String obtenerUbicacion() {
+		return this.ubicacion;
 	}
 }

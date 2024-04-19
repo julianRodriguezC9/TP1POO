edadf28520a236753ecb0349c5928b5fd024f42c
danilomedina12 <167130913+danilomedina12@users.noreply.github.com>
2024-04-19 17:06:28 -0300

    Update ControladorDonaciones.java

--- ControladorDonaciones.java
+++ ControladorDonaciones.java
@@ -8,13 +8,20 @@ public class ControladorDonaciones {
 	
 	public List<DonacionesUsuario> buscarCajas(int cantidad, LocalDate fecha, String ubicacion) {
 		List<DonacionesUsuario> cajasDisponibles = new ArrayList<>();
+		int cajasEncontradas = 0;
+		Iterator<DonacionesUsuario> iterator = donaciones.iterator();
 		
-		for(DonacionesUsuario donacion : donaciones) {
-			if(donacion.cantidad >= cantidad && donacion.fechaDisponibilidad.isAfter(fecha) && donacion.ubicacion.equals(ubicacion)) {
+		while(iterator.hasNext() && cajasEncontradas < cantidad) {
+			DonacionesUsuario donacion = iterator.next();
+			if(donacion.obtenerFechaDisponibilidad().isAfter(fecha) && donacion.obtenerUbicacion().equals(ubicacion)) {
 				cajasDisponibles.add(donacion);
+				cajasEncontradas += donacion.obtenerCantidad();
 			}
 		}
-		
+		if(cajasEncontradas < cantidad) {
+			System.out.println("No se encontraron suficientes cajas. Cajas encontradas: " + cajasEncontradas);
+			cajasDisponibles = null;
+		}
 		return cajasDisponibles;
 	}
 	
@@ -26,10 +33,28 @@ public class ControladorDonaciones {
 	 */
 	
 	public void reservarCajas(int cantidad, LocalDate fecha, String ubicacion) {
-		//TODO
+		//Sin probar
+		List<DonacionesUsuario> cajasParaReservar = buscarCajas(cantidad, fecha, ubicacion);
+		if(cajasParaReservar == null) {
+			System.out.println("No se pudo realizar la reserva. No hay suficientes cajas disponibles");
+			return;
+			}
+		int cajasReservadas = 0;
+		for(DonacionesUsuario donacion : cajasParaReservar) {
+			if(donacion.obtenerCantidad() >= (cantidad - cajasReservadas)) {
+				int cajasAReservar = cantidad - cajasReservadas;
+				cajasReservadas += cajasAReservar;
+			}else {
+				cajasReservadas += donacion.obtenerCantidad();
+				donacion.actualizarCantidad(0);
+			}
+		}
+		System.out.println("Reserva exitosa. Cajas reservadas: " + cajasReservadas);
 	}
 	
-	public void donarCajas(int cantidad, LocalDate fecha, String ubicacion, int idUsuario) {
-		//TODO
+	public void donarCajas(int cantidad, LocalDate fecha, String ubicacion, int idUsuario) {		
+		DonacionesUsuario nuevaDonacion = new DonacionesUsuario(cantidad, ubicacion, fecha, idUsuario);
+		donaciones.add(nuevaDonacion);
+		System.out.println("Donacion exitosa. Cantidad de cajas donadas: " + cantidad);
 	}
 }

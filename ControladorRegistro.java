a299af9d6e44016018baf57b9dcdf0034af19993
JulKing <148124705+JulKing@users.noreply.github.com>
2024-04-19 18:22:35 -0300

    Update ControladorRegistro.java

--- ControladorRegistro.java
+++ ControladorRegistro.java
@@ -4,6 +4,7 @@ public class ControladorRegistro {
 
 	
 	/*Mas problemas, incongruencias con el hecho de que se crearía un usuario con Strings y realmente son 
-	* String e Int
-	*/
+	String e Int
+	/Cuando se crea un usuario se debe crear tambien su DonacionUsario asociada */
+	
 }

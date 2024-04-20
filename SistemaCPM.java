bf361a80720c3d07baa123099dd6933e5e51b7cb
danilomedina12 <167130913+danilomedina12@users.noreply.github.com>
2024-04-20 18:20:20 -0300

    Update SistemaCPM.java

--- SistemaCPM.java
+++ SistemaCPM.java
@@ -1,5 +1,6 @@
 package sistemaCPM;
 
 public class SistemaCPM {
-  //TODO: Este sistema tiene que reunir instancias de los controladores de donacion y registro. 
-}
+	ControladorRegistro controladorRegistro = new ControladorRegistro();
+	ControladorDonaciones controladorDonaciones = new ControladorDonaciones();
+	}

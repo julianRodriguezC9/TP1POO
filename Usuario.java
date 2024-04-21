91cdc9a8aa58ab6356987ededc135e5b2bed3a85
alecolli1 <167717642+alecolli1@users.noreply.github.com>
2024-04-21 19:07:17 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -6,17 +6,18 @@ import java.util.*;
 public class Usuario {
 	int id;
 	String nombre;
+	ControladorDonaciones controlador;
 	
 	public Usuario(int id, String nombre) {
 		this.id = id;
 		this.nombre = nombre;
 	}
 
-    public void donarCajas(int cantidad, LocalDate fecha, String ubicacion, ControladorDonaciones controlador) {
+    public void donarCajas(int cantidad, LocalDate fecha, String ubicacion) {
         controlador.donarCajas(cantidad, fecha, ubicacion, this.id);
     }
 
-    public List<DonacionesUsuario> buscarCajas(int cantidad, LocalDate fecha, String ubicacion, ControladorDonaciones controlador) {
+    public List<DonacionesUsuario> buscarCajas(int cantidad, LocalDate fecha, String ubicacion) {
         return controlador.buscarCajas(cantidad, fecha, ubicacion);
     }
 }

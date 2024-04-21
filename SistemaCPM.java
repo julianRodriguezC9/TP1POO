931c4224f96b4c9151ff8b5577a5b13b1d375811
danilomedina12 <167130913+danilomedina12@users.noreply.github.com>
2024-04-20 21:57:44 -0300

    Update SistemaCPM.java

--- SistemaCPM.java
+++ SistemaCPM.java
@@ -3,4 +3,8 @@ package sistemaCPM;
 public class SistemaCPM {
 	ControladorRegistro controladorRegistro = new ControladorRegistro();
 	ControladorDonaciones controladorDonaciones = new ControladorDonaciones();
+
+	public List<DonacionesUsuario> realizarBusqueda(Usuario usuario, int cantidad, LocalDate fecha, String ubicacion){
+		return usuario.buscarCajas(cantidad, fecha, ubicacion, controladorDonaciones);
 	}
+}

2556e51347ccfcc14cf10ff9992f6f573c6fa8cf
danilomedina12 <167130913+danilomedina12@users.noreply.github.com>
2024-04-22 20:17:23 -0300

    Update Usuario.java
    Se regresa a la versión donde tenemos un 'private static void' (que maneja el identificador del Usuario) propio de la clase para la asignación en el caso de los Usuarios, es la forma buscada para no indicarlo en el constructor (el usuario se crea desde el sistemaCPM, dicho sistema no conocería que ID debe manejar un Usuario). 
    Se elimina la variable controlador del Usuario, el Usuario debe tener variables correspondientes, y el controlador será algo que manejará el sistemaCPM que hará las invocaciones pertinentes proveyendo dicho controlador.

--- Usuario.java
+++ Usuario.java
@@ -4,20 +4,23 @@ import java.time.*;
 import java.util.*;
 
 public class Usuario {
-	int id;
-	String nombre;
-	ControladorDonaciones controlador;
-	
-	public Usuario(int id, String nombre) {
-		this.id = id;
+	private static int nextId = 1;
+	int id; // Identificador único del usuario.
+	String nombre; // Nombre del usuario.
+ 
+	// Constructor de la clase Usuario.
+	public Usuario(String nombre) {
+		this.id = nextId++;
 		this.nombre = nombre;
 	}
-
-    public void donarCajas(int cantidad, LocalDate fecha, String ubicacion) {
+	
+	// Método para donar una cantidad específica de cajas para una mudanza.
+    public void donarCajas(int cantidad, LocalDate fecha, String ubicacion, ControladorDonaciones controlador) {
         controlador.donarCajas(cantidad, fecha, ubicacion, this.id);
     }
 
-    public List<DonacionesUsuario> buscarCajas(int cantidad, LocalDate fecha, String ubicacion) {
+    // Método para buscar una cantidad específica de cajas para una mudanza.
+    public List<DonacionesUsuario> buscarCajas(int cantidad, LocalDate fecha, String ubicacion, ControladorDonaciones controlador) {
         return controlador.buscarCajas(cantidad, fecha, ubicacion);
     }
 }

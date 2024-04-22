74bcace14633eac71a9e93158bfbe949216d3def
danilomedina12 <167130913+danilomedina12@users.noreply.github.com>
2024-04-22 19:58:32 -0300

    Update ControladorRegistro.java
    Se agrega una posible implementación de los métodos enviarDatosRegistro, validarDatosRegistro, crearUsuario, mostrarMensajeExito, mostrarMensajeError

--- ControladorRegistro.java
+++ ControladorRegistro.java
@@ -1,10 +1,42 @@
 package sistemaCPM;
 
-public class ControladorRegistro {
+import java.util.*;
 
+public class ControladorRegistro {
 	
-	/*Mas problemas, incongruencias con el hecho de que se crearía un usuario con Strings y realmente son 
-	String e Int
-	/Cuando se crea un usuario se debe crear tambien su DonacionUsario asociada */
+	List<Usuario> usuariosRegistrados = new ArrayList<>(); // Lista de todos los usuarios registrados.
+
+	 // Método para enviar los datos de registro de un nuevo usuario.
+	public void enviarDatosRegistro(String nombre, String ubicacion) {
+		if(validarDatosRegistro(nombre)) {
+			crearUsuario(nombre, ubicacion);
+			mostrarMensajeExito();
+		}else {
+			mostrarMensajeError();
+		}
+	}
 	
+	// Método para validar los datos de registro de un nuevo usuario.
+	public boolean validarDatosRegistro(String nombre) {
+		//Se valida si el nombre no es vacío 
+		//Acá debería validarse lo que realmente se tiene que validar para el registro
+		//(No me indicaron que es lo que se valida jajaja)
+		return !nombre.isEmpty();
+	}
+
+	// Método para crear un nuevo usuario.
+	public void crearUsuario(String nombre, String ubicacion) {
+		Usuario nuevoUsuario = new Usuario(nombre);
+		usuariosRegistrados.add(nuevoUsuario);
+	}
+	
+	// Método para mostrar un mensaje de éxito después de un registro exitoso.
+	public void mostrarMensajeExito() {
+		System.out.println("Registro exitoso.");
+	}
+	
+	// Método para mostrar un mensaje de error después de un registro fallido.
+	public void mostrarMensajeError() {
+		System.out.println("Error en el registro. Por favor, verifica los datos ingresados");
+	}
 }

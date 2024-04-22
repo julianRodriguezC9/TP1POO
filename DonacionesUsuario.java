8ca63016c25001bc322fbc31344a745e41fc30bd
danilomedina12 <167130913+danilomedina12@users.noreply.github.com>
2024-04-22 20:03:59 -0300

    Update DonacionesUsuario.java
    Se implementa el método obtenerCantidadCajasReservadas utilizada en el método reservarCajas de la clase ControladorDonaciones.

--- DonacionesUsuario.java
+++ DonacionesUsuario.java
@@ -5,12 +5,13 @@ import java.time.*;
 public class DonacionesUsuario {
 	private static int nextId = 1;
 	
-	int id;
-	int cantidad;
-	String ubicacion;
-	LocalDate fechaDisponibilidad;
-	int usuarioDonante;
+	int id; // Identificador único de la donación.
+	int cantidad; // Cantidad de cajas disponibles en la donación.
+	String ubicacion; // Ubicación de la donación.
+	LocalDate fechaDisponibilidad; // Fecha en la que las cajas estarán disponibles para la mudanza.
+	int usuarioDonante; // Identificador del usuario que donó las cajas.
 	
+	// Constructor de la clase DonacionesUsuario.
 	public DonacionesUsuario(int cantidad, String ubicacion, LocalDate fechaDisponibilidad, int usuarioDonante){
 		this.id = nextId++;
 		this.cantidad = cantidad; 
@@ -19,19 +20,32 @@ public class DonacionesUsuario {
 		this.usuarioDonante = usuarioDonante;
 		}
 	
+	// Método para actualizar la cantidad de cajas disponibles luego de la donación. 
 	public void actualizarCantidad(int nuevaCantidad) {
 		this.cantidad = nuevaCantidad;
 	}
 	
+	// Método para obtener la cantidad de cajas disponibles en la donación.
 	public int obtenerCantidad() {
 		return this.cantidad;
 	}
 
+	// Método para obtener la fecha en la que las cajas estarán disponibles para la mudanza.
 	public LocalDate obtenerFechaDisponibilidad() {
 		return this.fechaDisponibilidad;
 	}
 
+    // Método para obtener la ubicación de la donación.
 	public String obtenerUbicacion() {
 		return this.ubicacion;
+	}
+	
+    // Método para obtener la cantidad de cajas disponibles en la donación.
+	public int obtenerCantidadCajasReservadas(int cantidad) {
+	    if (this.cantidad >= cantidad) {
+	        return cantidad;
+	    } else {
+	        return this.cantidad;
+	    }
 	}
 }

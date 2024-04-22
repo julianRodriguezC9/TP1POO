a274818830fc07ed583170e19e63e218c018f316
danilomedina12 <167130913+danilomedina12@users.noreply.github.com>
2024-04-22 20:05:43 -0300

    Update SistemaCPM.java
    Se agrega las importaciones de las liberías java.util.* y java.time.* que se utilizan para el arreglo de lista y para la variable que contendrá una fecha.

--- SistemaCPM.java
+++ SistemaCPM.java
@@ -1,9 +1,13 @@
 package sistemaCPM;
 
-public class SistemaCPM {
-	ControladorRegistro controladorRegistro = new ControladorRegistro();
-	ControladorDonaciones controladorDonaciones = new ControladorDonaciones();
+import java.util.*;
+import java.time.*;
 
+public class SistemaCPM {
+	ControladorRegistro controladorRegistro = new ControladorRegistro(); // Controlador para el registro de usuarios.
+	ControladorDonaciones controladorDonaciones = new ControladorDonaciones(); // Controlador para las donaciones de cajas.
+	
+	// Método para realizar una búsqueda de cajas disponibles para una mudanza.
 	public List<DonacionesUsuario> realizarBusqueda(Usuario usuario, int cantidad, LocalDate fecha, String ubicacion){
 		return usuario.buscarCajas(cantidad, fecha, ubicacion, controladorDonaciones);
 	}

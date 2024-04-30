6e709317586205d4f5619abcdb3a6b33eb08c7dc
danilomedina12 <167130913+danilomedina12@users.noreply.github.com>
2024-04-29 23:05:47 -0300

    Modificación en ControladorRegistro.java
    Se vuelve estática la clase (no afecta a ningún test, ya que no se lo usa...)

--- ControladorRegistro.java
+++ ControladorRegistro.java
@@ -4,10 +4,10 @@ import java.util.*;
 
 public class ControladorRegistro {
 	
-	List<Usuario> usuariosRegistrados = new ArrayList<>(); // Lista de todos los usuarios registrados.
+	static List<Usuario> usuariosRegistrados = new ArrayList<>(); // Lista de todos los usuarios registrados.
 
 	 // Método para enviar los datos de registro de un nuevo usuario.
-	public void enviarDatosRegistro(String nombre) {
+	public static void enviarDatosRegistro(String nombre) {
 		if(validarDatosRegistro(nombre)) {
 			crearUsuario(nombre);
 			mostrarMensajeExito();
@@ -17,7 +17,7 @@ public class ControladorRegistro {
 	}
 	
 	// Método para validar los datos de registro de un nuevo usuario.
-	public boolean validarDatosRegistro(String nombre) {
+	public static boolean validarDatosRegistro(String nombre) {
 		//Se valida si el nombre no es vacío 
 		//Acá debería validarse lo que realmente se tiene que validar para el registro
 		//(No me indicaron que es lo que se valida jajaja)
@@ -25,18 +25,18 @@ public class ControladorRegistro {
 	}
 
 	// Método para crear un nuevo usuario.
-	public void crearUsuario(String nombre) {
+	public static void crearUsuario(String nombre) {
 		Usuario nuevoUsuario = new Usuario(nombre);
 		usuariosRegistrados.add(nuevoUsuario);
 	}
 	
 	// Método para mostrar un mensaje de éxito después de un registro exitoso.
-	private void mostrarMensajeExito() {
+	private static void mostrarMensajeExito() {
 		System.out.println("Registro exitoso.");
 	}
 	
 	// Método para mostrar un mensaje de error después de un registro fallido.
-	private void mostrarMensajeError() {
+	private static void mostrarMensajeError() {
 		System.out.println("Error en el registro. Por favor, verifica los datos ingresados");
 	}
 }

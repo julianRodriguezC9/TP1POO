f8f2da9a260eb16a8aca0fec4afe0c1c206d5d49
danilomedina12 <167130913+danilomedina12@users.noreply.github.com>
2024-05-02 11:45:38 -0300

    Update SistemaCPM.java
    Se borran las instancias de ControladorDonaciones y ControladorRegistro, son estáticos.

--- SistemaCPM.java
+++ SistemaCPM.java
@@ -4,8 +4,7 @@ import java.util.*;
 import java.time.*;
 
 public class SistemaCPM {
-	//ControladorRegistro controladorRegistro = new ControladorRegistro(); // Controlador para el registro de usuarios.
-	//ControladorDonaciones controladorDonaciones = new ControladorDonaciones(); // Controlador para las donaciones de cajas.
+
 	
 	// Método para realizar una búsqueda de cajas disponibles para una mudanza.
 	public List<DonacionesUsuario> realizarBusqueda(Usuario usuario, int cantidad, LocalDate fecha, String ubicacion){
